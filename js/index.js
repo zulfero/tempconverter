6e239097c75072658e060d9a4b9a19c04c15be2a
zulfa Ramadhan <zulfar436@gmail.com>
2024-03-06 11:36:29 +0300

    added the media querry method

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// let celcius = document.getElementById("celcius");
+// let kelvin = document.getElementById("kelvin");
+// let fahrenheit = document.getElementById("fahrenheit");
+
+// celcius.addEventListener("keydown", function (e) {
+//     if ((e.key === "9")) {
+//       console.log(e);
+//     console.log(e.key);
+//   }
+//   else(
+//     console.log("not 9")
+//   )
+// });
--- js/script.js
+++ js/script.js
@@ -2,46 +2,30 @@ let celcius = document.getElementById("celcius");
 let kelvin = document.getElementById("kelvin");
 let fahrenheit = document.getElementById("fahrenheit");
 
-celcius.addEventListener("input", function (e) {
+celcius.addEventListener("keydown", function (e) {
   console.log(e);
-  //   console.log(typeof(celciusValue))
-  //   if (e.key = "Enter") {
+    if (e.key === "Enter") {
   let celciusValue = +e.target.value;
   fahrenheit.value = (celciusValue * (5 / 9) + 32).toFixed(2);
   kelvin.value = (celciusValue + 273.15).toFixed(2);
-  //   }
+    }
 });
 
-fahrenheit.addEventListener("input", function (e) {
+fahrenheit.addEventListener("keydown", function (e) {
+    console.log(e);
+    if (e.key === "Enter") {
   let fahrenheitValue = +e.target.value;
   celcius.value = (fahrenheitValue - 32 * (5 / 9)).toFixed(2);
   kelvin.value = (fahrenheitValue + 273.15).toFixed(2);
+    }
 });
 
-kelvin.addEventListener("input", function (e) {
+kelvin.addEventListener("keydown", function (e) {
+    console.log(e);
+    if (e.key === "Enter") {
   let kelvinValue = +e.target.value;
   celcius.value = (kelvinValue - 273.15).toFixed(2);
   fahrenheit.Value = (kelvinValue + 273.15).toFixed(2);
+    }
 });
 
-// function temprature(temp){
-//     for(let i=0; i<=temp.length; i++){
-// if(temp<=80){
-
-// }
-
-//     }
-// }
-// let farenHeight = (celcius * 5) / 9 + 32;
-// enter.addEventListener("enter", function () {
-//   function temprature(temp) {
-//     for (let i = 0; i <= temp.length; i++) {
-//       if (temp <= 80) {
-//         console.log(temp[i] * 100);
-//         console.log(farenHeight);
-//       } else if (temp <= 70) {
-//         console.log(temp[i] * 10);
-//       }
-//     }
-//   }
-// });
